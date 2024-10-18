952471dfb5e4d65c63a0030b43b0d4f6b07cd141
PAULO <adrian.paulo.vaz@gmail.com>
2024-10-18 05:45:46 -0600

    primera pantalla:D

--- src/App.js
+++ src/App.js
@@ -19,19 +19,64 @@ function App() {
         <div className="img-viajes-recomendados">
           <h1>Viajes recomendados</h1>
         </div>
+        <div class="card-destino">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaY8WVzmRiNUWeBpm9CzcPEG4pLSNTN7sSvMTV9809QwZyxp7PW6gDhfTH23s-bJ1GP6Y&usqp=CAU" alt="Oaxaca de Juárez" />
+         <div className="card-destino-text">
+          <h4>Oaxaca</h4>
+          <p>Conoce Oaxaca, lugar turístico perfefcto para relajarse y disfrutar de sus hermosos paisajes y ambiente cálido.</p>
+         </div>
+        </div>
       </body>
       <footer class='viajes-recomendados-footer'>
-        <div className="viajes-recomendados-footer">
-        <i class="fa-brands fa-twitter icon-twitter"></i>
+        <div class="viajes-recomendados-footer-icons">
+          <div>
+            <i class="fa-brands fa-twitter icon-twitter"></i>
+          </div>
+          <div>
+            <i class="fa-brands fa-instagram icon-instagram"></i>
+          </div>
+          <div>
+            <i class="fa-brands fa-youtube icon-youtube"></i>
+          </div>
+          <div>
+            <i class="fa-brands fa-linkedin icon-linkedin"></i>
+          </div>
         </div>
-        <div>
-
+        <div class="viajes-recomendados-footer-text">
+          <h6>Experiencia</h6>
+          <ul class="lista-footer">
+            <li>UI design</li>
+            <li>UX design</li>
+            <li>Wireframing</li>
+            <li>Diagramming</li>
+            <li>Brainstorming</li>
+            <li>Online whiteboard</li>
+            <li>Team collaboration</li>
+          </ul>
         </div>
-        <div>
-
+        <div class="viajes-recomendados-footer-text">
+          <h6>Explore</h6>
+          <ul class="lista-footer">
+            <li>Design</li>
+            <li>Prototyping</li>
+            <li>Development features</li>
+            <li>Design systems</li>
+            <li>Collaboration features</li>
+            <li>Design process</li>
+            <li>FigJam</li>
+          </ul>
         </div>
-        <div>
-
+        <div class="viajes-recomendados-footer-text">
+          <h6>Resources</h6>
+          <ul class="lista-footer">
+            <li>Blog</li>
+            <li>Best Practices</li>
+            <li>Colors</li>
+            <li>Color wheel</li>
+            <li>Support</li>
+            <li>Developers</li>
+            <li>Resource library</li>
+          </ul>
         </div>
 
       </footer>
