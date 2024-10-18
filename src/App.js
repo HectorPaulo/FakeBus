66db1caf6ca58200978bac09120dd5ea745b8d29
Hector Paulo <adrian.paulo.vaz@gmail.com>
2024-10-17 22:35:54 -0600

    correcciones mayores

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul className="nav-list">
+            <li><a href="/">Inicio</a></li>
+            <li><a href="/">Nosotros</a></li>
+            <li><a href="/">Viajes</a></li>
+            <li><a href="/">Contacto</a></li>
+            <button className="ingresar-button">Ingresar</button>
+            <button className="acceder-button">Acceder</button>
+          </ul>
+        </nav> 
       </header>
+      <body class="viajes-recomendados-body">
+        <div className="img-viajes-recomendados">
+          <h1>Viajes recomendados</h1>
+        </div>
+      </body>
+      <footer class='viajes-recomendados-footer'>
+        <div className="viajes-recomendados-footer">
+        <i class="fa-brands fa-twitter icon-twitter"></i>
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+
+      </footer>
     </div>
   );
 }
